beede61b63acff81c711a9fcfacd16b3e2cd5fe7
tomori-k <108829509+tomori-k@users.noreply.github.com>
2025-03-14 17:00:47 +0000

    UPDATE

--- build_reset.sh
+++ build_reset.sh
@@ -1,2 +1,4 @@
-west build -d build/reset -b seeeduino_xiao_ble -- -DSHIELD=settings_reset -DZMK_CONFIG="/workspaces/zmk-config/config"
-cp /workspaces/zmk/app/build/reset/zephyr/zmk.uf2 build/reset.uf2
+mkdir -p build
+cd /workspaces/zmk/app
+west build -d build/reset -b seeeduino_xiao_ble -- -DSHIELD=settings_reset -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config/zmk-driver-paw32xx"
+cp /workspaces/zmk/app/build/reset/zephyr/zmk.uf2 /workspaces/zmk-config/build/reset.uf2
