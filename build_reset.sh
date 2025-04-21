e0df6e2f82cb627781a701a644bd9d73b12370a8
tomori-k <108829509+tomori-k@users.noreply.github.com>
2025-04-21 17:39:44 +0000

    fix

--- build_reset.sh
+++ build_reset.sh
@@ -1,4 +1,4 @@
 mkdir -p build
 cd /workspaces/zmk/app
-west build -d build/reset -b seeeduino_xiao_ble -- -DSHIELD=settings_reset -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config/zmk-driver-paw32xx"
+west build -d build/reset -b seeeduino_xiao_ble -- -DSHIELD=settings_reset -DZMK_CONFIG="/workspaces/zmk-config/config" -DZMK_EXTRA_MODULES="/workspaces/zmk-config/zmk-pmw3610-driver"
 cp /workspaces/zmk/app/build/reset/zephyr/zmk.uf2 /workspaces/zmk-config/build/reset.uf2
